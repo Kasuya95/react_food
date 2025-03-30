c4229ea7d2333b0d08b5adefd1e3ca592583dc2a
Shiku End <shikuend@MacBook-Air-khxng-Shiku.local>
2025-03-30 17:03:06 +0700

    1

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
-  base:"/react_food/",
+  server: {
+    port: 3000 // หรือ port อื่นๆ ที่ Render รองรับ
+  },
   daisyui: {
     themes: ["caramellette"], 
   },
