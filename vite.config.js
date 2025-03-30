e52326427bc95ea3a661e89b4128c1e4777e5e66
Shiku End <shikuend@MacBook-Air-khxng-Shiku.local>
2025-03-30 16:55:41 +0700

    new

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base:"/react_food",
   daisyui: {
     themes: ["caramellette"], 
   },
